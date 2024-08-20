3d4cf4f44561a239282d2f396c82a73a72bd9d0a
ivan.sigaev <ivan.sigaev@check24.de>
2024-08-20 12:30:25 +0200

    feat: add the 2nd variant

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 module.exports = function towelSort(matrix) {
     if (!matrix || matrix.length === 0) return [];
-    let result = [];
-    for (let i = 0; i < matrix.length; i++) {
-        if (i % 2 === 0) {
-            for (let j = 0; j < matrix[i].length; j++) {
-                result.push(matrix[i][j]);
-            }
-        } else {
-            for (let j = matrix[i].length - 1; j >= 0; j--) {
-                result.push(matrix[i][j]);
-            }
-        }
-    }
 
-    return result;
+    return matrix.reduce((acc, row, index) => {
+        if (index % 2 === 0) {
+            // For even index rows, add the row as is
+            return acc.concat(row);
+        } else {
+            // For odd index rows, add the row in reverse order
+            return acc.concat(row.reverse());
+        }
+    }, []);
 }
